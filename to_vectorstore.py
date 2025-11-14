8990c1c2e4d3c29286e534c844daf61b72461f9a
uc-cjkhoh <cj_khoh@unifiedcomms.com>
2025-11-14 15:55:42 +0800

    minor change

--- to_vectorstore.py
+++ to_vectorstore.py
@@ -12,7 +12,7 @@ from data_loader.connection import Database
 from src.data_loader.preprocessing import text_normalize 
 from src.vector_database.vectorstore import VectorStore
 from src.config_folder.config_loader import get_config
-from src.utils.util import create_required_folder_file, update_metadata 
+from src.utils.util import create_required_folder_file, update_metadata, generate_metadata
 
 
 @task(cache_policy=NO_CACHE)  
@@ -35,24 +35,45 @@ def setup_environment(config: dict):
 @flow(name='Ingestion to Vectorstore')
 def main():  
     config = get_config() 
+    
     create_required_folder_file(config) 
     
     db, embedding_model, vectorstore = setup_environment(config)    
+    
     metadata = db.run_query(config.metadata.query, columns=config.metadata.column_name) 
+    
     update_metadata(config, metadata)
     
     for i in range(len(metadata)):  
+        # modify query based on current metadata
         data_query = config.data.query.format(*metadata.iloc[i]) 
-        data = db.run_query(data_query, columns=config.data.column_name)
-        data_metadata = data[config.data.metadata_column].to_dict(orient='records')
+        
+        # get message from mysql
+        data = db.run_query(data_query, columns=config.data.column_name) 
          
+        # normalize text
         data = text_normalize(data, target_column=config.data.target_column)
+        
+        # convert dataframe to list
         messages = data[config.data.target_column].to_list()
          
+        # perform text embedding
         embeddings = np.asarray(embedding_model.embed_documents(messages))
-         
+        
+        # generate metadata for each row
+        data_metadata = pd.concat([
+            data[config.data.metadata_column],
+            pd.DataFrame({
+                'label': [None] * len(data),
+                'confidence_score': [None] * len(data),
+                'label_status': ['unlabeled'] * len(data)
+            })
+        ], axis=1)
+        
+        # write embeddings into faiss
         vectorstore.write_to_vectorstore(zip(messages, embeddings), embedding_model, data_metadata) 
          
+        # save progress status
         update_metadata(config)
                     
     db.close_connection()  
